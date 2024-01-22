e23145984444a05dbc25bb6cd8eef299171e18c4
LuizCantalice <luizcantalice@estudante.ufscar.br>
2024-01-21 22:00:15 -0300

    Monster move script

---
+++ Assets/Ghost_move_blue.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost_move_blue : MonoBehaviour
+{
+
+  
+    
+    private Vector2 moveDirection;
+    public Rigidbody2D monsterRb;
+    public float speed = 2f;
+    
+
+
+    void movemment ()
+    {
+       monsterRb.velocity = new Vector2(0, moveDirection.y * speed);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveDirection =  new Vector2(0,-1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movemment();
+    }
+    
+    
+}
