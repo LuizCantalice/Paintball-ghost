f732a4b5535bf0d3c4044ff035eb71b3974ded7e
LuizCantalice <luizcantalice@estudante.ufscar.br>
2024-01-22 09:21:35 -0300

    Next step

---
+++ Assets/CollisionManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour
+{
+    public int vidas;
+    public PolygonCollider2D pc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Colisao){
+            vidas--
+            moreu(fantasma)
+        }*/
+
+        
+    }
+}
---
+++ Assets/MataFantasma.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MataFantasma : MonoBehaviour
+{
+    public BoxCollider2D bc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(colisao)
+            mata(fantasma)*/
+    }
+}
